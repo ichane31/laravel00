56c3563238a08efb546d16beb88b4eefd218ece9
ichane31 <roukeyaassouma@gmail.com>
2022-04-19 21:02:40 +0000

    correction

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -48,11 +48,11 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Déja inscrit?') }}
                 </a>
 
                 <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Inscription') }}
                 </x-jet-button>
             </div>
         </form>
